dfcae69c76641ffd001058fc9111be9e060a17d2
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-26 13:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function expandPanel(panelId) {
+    // Reset all panels
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => panel.classList.remove('active'));
+
+    // Expand the clicked panel
+    const clickedPanel = document.getElementById(panelId);
+    clickedPanel.classList.add('active');
+  }
